6155622510248946bb0ee2fb54808b774e0521ae
ProtoSG <diegoalbertosalazar177@gmail.com>
2024-05-31 12:32:48 -0500

    add turso

--- app.py
+++ app.py
@@ -1,53 +1,87 @@
+import os 
+from dotenv import load_dotenv
 from flask import Flask, request, jsonify
-from models import db, Todo
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import Session, sessionmaker
 
+load_dotenv()
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db.init_app(app)
+TURSO_DATABASE_URL = os.environ.get('TURSO_DATABASE_URL')
+TURSO_AUTH_TOKEN = os.environ.get('TURSO_AUTH_TOKEN')
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
 
-@app.route('/todos', methods=['GET'])
-def get_todos():
-    todos = Todo.query.all()
-    return jsonify([todo.to_dict() for todo in todos])
+dbUrl = f"sqlite+{TURSO_DATABASE_URL}/?authToken={TURSO_AUTH_TOKEN}&secure=true"
 
-@app.route('/todos/<int:todo_id>', methods=['GET'])
-def get_todo(todo_id):
-    todo = Todo.query.get_or_404(todo_id)
-    return jsonify(todo.to_dict())
+engine = create_engine(dbUrl, echo=True)
 
-@app.route('/todos', methods=['POST'])
-def create_todo():
-    data = request.get_json()
-    new_todo = Todo(
-        title=data.get('title'),
-        description=data.get('description', '')
-    )
-    db.session.add(new_todo)
-    db.session.commit()
-    return jsonify(new_todo.to_dict()), 201
+@app.route('/', methods=['GET'])
+def get_producto():
+    session = Session(engine)
+    sql = text("""
+        SELECT p.producto_id, p.nombre, p.precio, p.descripcion, p.imagen_url, c.categoria_id, c.nombre AS nombre_categoria
+        FROM Producto p
+        JOIN Categoria c ON p.categoria_id = c.categoria_id;
+    """)
+                
+    datos = session.execute(sql)
+    productos = []
+    for fila in datos:
+        producto = {
+            'id': fila[0],
+            'nombre': fila[1],
+            'precio': fila[2],
+            'descripcion': fila[3],
+            'imagen_url': fila[4],
+            'categoria': {
+                'id' : fila[5],
+                'nombre' : fila[6]
+            }
+        }
+        productos.append(producto)
+    return jsonify(productos)
 
-@app.route('/todos/<int:todo_id>', methods=['PUT'])
-def update_todo(todo_id):
-    todo = Todo.query.get_or_404(todo_id)
-    data = request.get_json()
-    todo.title = data.get('title', todo.title)
-    todo.description = data.get('description', todo.description)
-    todo.done = data.get('done', todo.done)
-    db.session.commit()
-    return jsonify(todo.to_dict())
+# @app.before_first_request
+# def create_tables():
+#     db.create_all()
 
-@app.route('/todos/<int:todo_id>', methods=['DELETE'])
-def delete_todo(todo_id):
-    todo = Todo.query.get_or_404(todo_id)
-    db.session.delete(todo)
-    db.session.commit()
-    return '', 204
+# @app.route('/todos', methods=['GET'])
+# def get_todos():
+#     todos = Todo.query.all()
+#     return jsonify([todo.to_dict() for todo in todos])
+#
+# @app.route('/todos/<int:todo_id>', methods=['GET'])
+# def get_todo(todo_id):
+#     todo = Todo.query.get_or_404(todo_id)
+#     return jsonify(todo.to_dict())
+#
+# @app.route('/todos', methods=['POST'])
+# def create_todo():
+#     data = request.get_json()
+#     new_todo = Todo(
+#         title=data.get('title'),
+#         description=data.get('description', '')
+#     )
+#     db.session.add(new_todo)
+#     db.session.commit()
+#     return jsonify(new_todo.to_dict()), 201
+#
+# @app.route('/todos/<int:todo_id>', methods=['PUT'])
+# def update_todo(todo_id):
+#     todo = Todo.query.get_or_404(todo_id)
+#     data = request.get_json()
+#     todo.title = data.get('title', todo.title)
+#     todo.description = data.get('description', todo.description)
+#     todo.done = data.get('done', todo.done)
+#     db.session.commit()
+#     return jsonify(todo.to_dict())
+#
+# @app.route('/todos/<int:todo_id>', methods=['DELETE'])
+# def delete_todo(todo_id):
+#     todo = Todo.query.get_or_404(todo_id)
+#     db.session.delete(todo)
+#     db.session.commit()
+#     return '', 204
 
 if __name__ == '__main__':
     app.run(debug=True)
--- models.py
+++ models.py
@@ -1,18 +1,18 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(80), nullable=False)
-    description = db.Column(db.String(200), nullable=True)
-    done = db.Column(db.Boolean, default=False, nullable=False)
-
-    def to_dict(self):
-        return {
-            'id': self.id,
-            'title': self.title,
-            'description': self.description,
-            'done': self.done
-        }
-
+# from flask_sqlalchemy import SQLAlchemy
+#
+# db = SQLAlchemy()
+#
+# class Todo(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     title = db.Column(db.String(80), nullable=False)
+#     description = db.Column(db.String(200), nullable=True)
+#     done = db.Column(db.Boolean, default=False, nullable=False)
+#
+#     def to_dict(self):
+#         return {
+#             'id': self.id,
+#             'title': self.title,
+#             'description': self.description,
+#             'done': self.done
+#         }
+#
